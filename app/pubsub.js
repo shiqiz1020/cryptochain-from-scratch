441fa0eb8ee11bef521e1d0205246b3a39fecf4f
shiqiz1020 <shiqiz1020@gmail.com>
2021-06-21 17:09:43 +0800

    Transaction pool, sync transaction complete:

--- app/pubsub.js
+++ app/pubsub.js
@@ -3,12 +3,15 @@ const redis = require('redis');
 
 const CHANNELS = {
     TEST:'TEST',
-    BLOCKCHAIN: 'BLOCKCHAIN'
+    BLOCKCHAIN: 'BLOCKCHAIN',
+    TRANSACTION: 'TRANSACTION'
 };
 
 class PubSub {
-    constructor({ blockchain }) {
+    constructor({ blockchain, transactionPool, wallet }) {
         this.blockchain = blockchain;
+        this.transactionPool = transactionPool;
+        this.wallet = wallet;
 
         this.publisher = redis.createClient();
         this.subscriber = redis.createClient();
@@ -26,8 +29,19 @@ class PubSub {
 
         const parsedMessage = JSON.parse(message);
 
-        if (channel === CHANNELS.BLOCKCHAIN) {
-            this.blockchain.replaceChain(parsedMessage);
+        switch(channel) {
+            case CHANNELS.BLOCKCHAIN:
+                this.blockchain.replaceChain(parsedMessage);
+                break;
+            case CHANNELS.TRANSACTION:
+                if (!this.transactionPool.existingTransaction({
+                    inputAddress: this.wallet.publicKey
+                })) {
+                    this.transactionPool.setTransaction(parsedMessage);
+                }
+                break;
+            default:
+                return;
         }
     }
 
@@ -49,6 +63,13 @@ class PubSub {
         this.publish({
             channel: CHANNELS.BLOCKCHAIN,
             message: JSON.stringify(this.blockchain.chain)
+        });
+    }
+
+    broadcastTransaction(transaction) {
+        this.publish({
+            channel: CHANNELS.TRANSACTION,
+            message: JSON.stringify(transaction)
         });
     }
 }
